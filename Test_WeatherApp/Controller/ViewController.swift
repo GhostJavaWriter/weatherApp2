93dd04f2dd4e0beb6519fe119dc8c180ec2ed2f4
Bair Nadtsalov <nadtsalov@gmail.com>
2021-02-16 20:09:23 +0800

    docs: add .gitignore

--- Test_WeatherApp/Controller/ViewController.swift
+++ Test_WeatherApp/Controller/ViewController.swift
@@ -150,6 +150,7 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         
         if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
             vc.city = currentCity
+            
             navigationController?.pushViewController(vc, animated: true)
         }
     }
